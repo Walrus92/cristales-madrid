f66f4dcf4853b3dc86492a52a204f35621993faa
Morzo <miguelmorci@gmail.com>
2025-11-15 13:18:38 +0100

    enable github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,44 +1,9 @@
-import { site } from "./config";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
-  async headers() {
-    return [
-      {
-        source: "/robots.txt",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "text/plain; charset=utf-8",
-          },
-        ],
-      },
-      {
-        source: "/sitemap.xml",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/xml; charset=utf-8",
-          },
-        ],
-      },
-    ];
+  images: {
+    unoptimized: true,
   },
-
-  async redirects() {
-    return [];
-  },
-
-  async rewrites() {
-    return [];
-  },
-
-  // Inyectamos los valores dinámicos al exportar el sitio
-  experimental: {
-    typedRoutes: false,
-  },
-
 };
 
-export default nextConfig;
+module.exports = nextConfig;
